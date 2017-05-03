30d77a14246fdf4bd7005b4acf582b10307d4e20
Vyacheslav.Vanin <vyacheslav.vanin@gmail.com>
2017-05-03 20:02:13 +0300

    add plugin auto download and install

--- install.sh
+++ install.sh
@@ -3,3 +3,23 @@ cp .Xresources $HOME/
 cp .dwmrc $HOME/
 cp .vimrc $HOME/
 cp .clang-format $HOME/
+
+# Download vim-plug if not exist
+if [ ! -f ~/.vim/autoload/plug.vim ]; then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# Install plugins
+if command -v vim ; then
+    vim +PlugInstall +PlugUpdate +qall
+fi
+
+if command -v nvim ; then
+    nvim +PlugInstall +PlugUpdate +qall
+fi
